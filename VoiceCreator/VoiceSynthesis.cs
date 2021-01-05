ba239d47c025098dcee7199b246b86858320edd2
Budochka <budochka@bk.ru>
2021-01-05 16:45:28 +0300

    Add project files.

---
+++ VoiceCreator/VoiceSynthesis.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Microsoft.Speech.Synthesis;
+using NLog;
+
+namespace VoiceCreator
+{
+    class VoiceSynthesis
+    {
+        private Logger _logs;
+        private SpeechSynthesizer _speechSynthesizer;
+
+        public VoiceSynthesis(Logger log)
+        { 
+            _logs = log;
+            _logs.Debug("Initializing speech synthesizer");
+
+            try
+            {
+                _speechSynthesizer = new SpeechSynthesizer();
+            }
+            catch (Exception ex)
+            {
+                _logs.Error(ex, "Error creating speech synthesizer");
+            }
+        }
+
+        public bool Text2File(in string text, out string filepath)
+        {
+            filepath = GetTempFileName();
+
+            try
+            {
+                _speechSynthesizer.SetOutputToWaveFile(filepath);
+            }
+            catch (Exception ex)
+            {
+                _logs.Error(ex, "Error converting text to wave file");
+            }
+        }
+    }
+}
