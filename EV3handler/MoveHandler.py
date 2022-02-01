ec0b9e768c3a3f0c6cada8577674a46f6ec3d569
Budochka <budochka@bk.ru>
2022-02-01 22:03:41 +0300

    Fixing byte to int conversion

--- EV3handler/MoveHandler.py
+++ EV3handler/MoveHandler.py
@@ -1,4 +1,5 @@
 import logging
+from pickle import TRUE
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 
 def callback_move(ch, method, properties, body):
@@ -7,17 +8,17 @@ def callback_move(ch, method, properties, body):
 
     if (method.routing_key.find('.turn') != -1):
         logging.info('Turn message processed')
-        degree = int.from_bytes(body[0], byteorder ='big')
-        torque = int.from_bytes(body[4], byteorder ='big')
+        degree = int.from_bytes(body[0:3], byteorder ='little')
+        torque = int.from_bytes(body[4:7], byteorder ='little')
 
         tank_drive.on_for_seconds(SpeedPercent(-torque), SpeedPercent(torque), 1)
         return
 
     if (method.routing_key.find('.distance') != -1):
         logging.info('Move message processed')
-        distance = int.from_bytes(body[0], byteorder ='big')
-        torque = int.from_bytes(body[4], byteorder ='big')
+        distance = int.from_bytes(body[0:3], byteorder ='little', signed=TRUE)
+        torque = int.from_bytes(body[4:7], byteorder ='little')
 
 
-        tank_drive.on_for_seconds(SpeedPercent(torque * distance), SpeedPercent(torque * distance), 1)
+        tank_drive.on_for_seconds(SpeedPercent(torque), SpeedPercent(torque), distance/60)
         return
